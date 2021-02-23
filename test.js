b7f2b9e332958a298bbe07b35aab27b7e3c4f0cb
CaseyK9 <40074095+HyenaLtd@users.noreply.github.com>
2021-02-23 09:45:28 -0500

    update

--- test.js
+++ test.js
@@ -1,28 +1,11 @@
 "use strict";
-
-/**
- * This test file is used to test the basic functionality of the dashbaord.
- * It is also here to serve as an example implementation of the module.
- */
 require("dotenv").config();
-const { Client } = require("discord.js");
-const Dashboard = require("./lib/index");
 
-/**
- * Create an instance of a Discord client
- */
-const client = new Client();
-const dashboard = new Dashboard(client, {
-    port: 8080,
+const Dashboard = require("./index");
+
+Dashboard({
+    port: 5000,
     clientID: process.env.clientID,
     clientSecret: process.env.clientSecret,
-    redirectURI: `http://localhost:8080/auth/discord/callback`,
-    websiteDomain: "http://localhost:8080",
-    mongodbURI: process.env.mongoDBURL,
+    //callbackURL: process.env.callbackURL
 });
-
-client.on("ready", () => {
-    dashboard.run();
-});
-
-client.login(process.env.TOKEN);
